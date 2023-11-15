db0f29b232e8f69fbb1e27dca323fc676bdd6f5a
TonyMyrato <tonymyrato@TonyMyratos-Air.MyMiFi>
2023-11-14 23:25:17 -0800

    class

---
+++ startClass.ts
@@ -0,0 +1,58 @@
+class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+
+  run(speed) {
+    this.speed = speed;
+    alert(`${this.name} бежит со скоростью ${this.speed}.`);
+  }
+
+  stop() {
+    this.speed = 0;
+    alert(`${this.name} стоит неподвижно.`);
+  }
+}
+
+class Rabbit extends Animal {
+  hide() {
+    alert(`${this.name} прячется!`);
+  }
+
+  stop() {
+    super.stop(); // вызываем родительский метод stop
+    this.hide(); // и затем hide
+  }
+}
+
+let rabbit = new Rabbit("Белый кролик");
+
+rabbit.run(5); // Белый кролик бежит со скоростью 5.
+rabbit.stop(); // Белый кролик стоит. Белый кролик прячется!
+class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+  // ...
+}
+
+class Rabbit extends Animal {
+
+  constructor(name, earLength) {
+    this.speed = 0;
+    this.name = name;
+    this.earLength = earLength;
+  }
+
+  // ...
+}
+
+// Не работает!
+let rabbit = new Rabbit("Белый кролик", 10); // Error: this is not defined.
+Упс! При создании кролика – ошибка! Что не так?
+
+  Если коротко, то:
+
+Конструкторы в наследуемых классах должны обязательно вызывать super(...), и (!) делать это перед использованием this..
